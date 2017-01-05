c583a54ac7b861b5bec9911be08057c2253397aa
Arne Coucheron <arco68@gmail.com>
2017-01-05 05:01:27 +0100

    serranodsdd: Rebrand to LineageOS
    Change-Id: I54098315be77649be181a38f8ea981d688ae0124

--- lineage.mk
+++ lineage.mk
@@ -1,4 +1,4 @@
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/serranodsdd/full_serranodsdd.mk)
@@ -6,4 +6,4 @@ $(call inherit-product, device/samsung/serranodsdd/full_serranodsdd.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranodsdd TARGET_DEVICE=serranods BUILD_FINGERPRINT="samsung/serranodsdd/serranods:4.4.2/KOT49H/I9192DDUCOF2:user/release-keys" PRIVATE_BUILD_DESC="serranodsdd-user 4.4.2 KOT49H I9192DDUCOF2 release-keys"
 
 PRODUCT_DEVICE := serranodsdd
-PRODUCT_NAME := cm_serranodsdd
+PRODUCT_NAME := lineage_serranodsdd
