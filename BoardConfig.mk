da7fb1a72118372533d8c6f07dbda59d6184fa56
Arne Coucheron <arco68@gmail.com>
2017-04-13 12:20:20 +0200

    serranodsdd: Add TARGET_PROVIDES_LIBLIGHT
    Change-Id: I9ff63d3557592137af973c1ebdcd5f831c406ac8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,6 +33,9 @@ TARGET_OTA_ASSERT_DEVICE := serranods,serranodsdd,serranodsub,serranodsxx
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8930_serrano_eur_3g_defconfig
 
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
 # BLN
 BOARD_HAVE_GENERIC_BLN := true
 
